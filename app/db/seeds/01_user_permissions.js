023544bb9bd6fda8b194588987af0b3df608244f
daviid <david.hany129@gmail.com>
2020-05-09 17:01:17 +0200

    add user endpoints to permissions

---
+++ app/db/seeds/01_user_permissions.js
@@ -0,0 +1,48 @@
+exports.seed = async (knex, Promise) => {
+  await knex('role_permission').del();
+  await knex('permission').where('model', 'user').del();
+  await knex('permission').insert([
+    {
+      name: 'List',
+      key: 'list',
+      model: 'user',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Find',
+      key: 'find',
+      model: 'user',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Insert',
+      key: 'insert',
+      model: 'user',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Update',
+      key: 'update',
+      model: 'user',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Delete',
+      key: 'delete',
+      model: 'user',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'all',
+      key: 'all',
+      model: 'user',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+  ]);
+};
--- app/modules/users/user-controller.js
+++ app/modules/users/user-controller.js
@@ -18,7 +18,7 @@ class UserController extends Controller {
    *
    * @return  {array}
    */
-  static skipPermissionCheck = ['insert', 'update', 'find', 'all', 'paginate', 'delete'];
+  static skipPermissionCheck = [];
 
   /**
    * Init new object
