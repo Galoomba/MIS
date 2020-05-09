1c90c57b5169ec658d89adb08c17af62bff4a305
daviid <david.hany129@gmail.com>
2020-05-09 18:22:45 +0200

    add permission and role seeds

---
+++ app/db/seeds/02_roles_permissions.js
@@ -0,0 +1,48 @@
+
+exports.seed = async (knex, Promise) => {
+  await knex('permission').where('model', 'role').del();
+  await knex('permission').insert([
+    {
+      name: 'List',
+      key: 'all',
+      model: 'role',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Find',
+      key: 'find',
+      model: 'role',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Assign',
+      key: 'assign',
+      model: 'role',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Insert',
+      key: 'insert',
+      model: 'role',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Update',
+      key: 'update',
+      model: 'role',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Delete',
+      key: 'delete',
+      model: 'role',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+  ]);
+};
---
+++ app/db/seeds/03_permissions_permissions.js
@@ -0,0 +1,20 @@
+
+exports.seed = async (knex, Promise) => {
+  await knex('permission').where('model', 'permission').del();
+  await knex('permission').insert([
+    {
+      name: 'List',
+      key: 'all',
+      model: 'permission',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+    {
+      name: 'Find',
+      key: 'find',
+      model: 'permission',
+      created_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+      updated_at: require('moment')().format('YYYY-MM-DD hh:mm:ss'),
+    },
+  ]);
+};
