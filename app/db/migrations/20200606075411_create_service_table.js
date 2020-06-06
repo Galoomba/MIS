851f818d3fdb42273a7c1af322d1a15ea4cd2a56
daviid <david.hany129@gmail.com>
2020-06-06 20:17:38 +0200

    update service table

--- app/db/migrations/20200606075411_create_service_table.js
+++ app/db/migrations/20200606075411_create_service_table.js
@@ -3,7 +3,14 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('services', (table) => {
     table.increments('id');
     /** Entities */
+    table.integer('church_id').nullable().unsigned();
     table.string('name', 200).unique().notNullable();
+    table.string('type', 200).unique().notNullable();
+    table.dateTime('date').notNullable();
+    table.integer('capacity').notNullable();
+    table.integer('available_seats').notNullable();
+    table.foreign('church_id').references('churches.id');
+
 
     /** Basics */
     table.boolean('deleted').default(0);
